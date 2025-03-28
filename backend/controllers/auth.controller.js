d1bae97597ccac22139926261c70cc7f03aa26cc
Nick <nikysonpebam12345@gmail.com>
2025-03-28 23:23:44 +0530

    login status code updated

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -80,7 +80,7 @@ export const login = async (req, res) => {
             })
         }
         else {
-            res.status(401).json({message: 'Invalid credentials.'})
+            res.status(400).json({message: 'Invalid credentials.'})
         }
     } catch (error) {
         res.status(500).json({message: error.message});
